fd9662019275d8bd7544a345a3f1b09017e39816
KINOSHITA-KEI-TA <kei.quality3@gmail.com>
2020-09-05 17:38:46 +0900

    非同期の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "comments#index"
+  resources :explanations,only:[:index]
   resources :child_secrets,only: [:index,:create]
   resources :comments do
     resources :messages,only: [:create] do
