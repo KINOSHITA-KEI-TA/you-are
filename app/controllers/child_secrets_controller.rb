be526f4b512bae2b4a32d601fcdb39894478093a
KINOSHITA-KEI-TA <kei.quality3@gmail.com>
2020-09-11 00:06:20 +0900

    トップ画面・アップデートの追加

--- app/controllers/child_secrets_controller.rb
+++ app/controllers/child_secrets_controller.rb
@@ -6,11 +6,14 @@ class ChildSecretsController < ApplicationController
   def create
     @secret = Secret.new(secret_params)
     if @secret.save
-      redirect_to root_path, notice: "おくりました" 
+      redirect_to new_child_secret_path, notice: "おくりました" 
     else
       redirect_to action: "new"
       flash[:notice] = "失敗しました" 
     end
+  end
+
+  def new
   end
 
   private
