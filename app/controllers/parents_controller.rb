3a958b9f37dbc43c629432c036a6c36599cb1724
KINOSHITA-KEI-TA <kei.quality3@gmail.com>
2020-12-01 11:34:31 +0900

    parentフィールド実装完了

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -2,15 +2,20 @@ class ParentsController < ApplicationController
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
-
   def index
-    
+    @comments = Comment.all.order("created_at DESC")
   end
 
-  
+  def new
+    @comment = Comment.new
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:school_name])
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:name, :age, :tittle, :text)
   end
 end
