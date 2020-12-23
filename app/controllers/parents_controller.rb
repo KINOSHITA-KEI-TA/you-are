767b504bf313371788589df6ea64fadeb0d0a5ba
KINOSHITA-KEI-TA <kei.quality3@gmail.com>
2020-12-23 10:26:38 +0900

    ログイン機能分岐

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -17,6 +17,7 @@ class ParentsController < ApplicationController
     end
   end
 
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:school_name])
   end
